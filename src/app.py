333714a4db6b132cdeda89841f2af5b21484152c
Antonin Plard <antonin.plard@proton.me>
2026-06-24 11:02:53 +0200

    move app.py to root because of import issues

---
+++ src/app.py
@@ -0,0 +1,23 @@
+import sys
+from pathlib import Path
+
+sys.path.insert(0, str(Path(__file__).parent / "ui"))
+
+import streamlit as st
+
+st.title("Chenin")
+st.badge("PTAL")
+st.markdown("Un outil d'analyse de donnée pour les rapports de `Génie200`.")
+st.info("En cours de developpement !")
+st.divider()
+
+pages = {
+    "Outils": [
+        st.Page("ui/pages/report.py", title="Extracteur de rapport"),
+        st.Page("ui/pages/synthesis.py", title="Constructeur de synthese"),
+        # st.Page("ui/pages/carotte.py", title="Visualisation carotte"),
+    ]
+}
+
+pg = st.navigation(pages)
+pg.run()
